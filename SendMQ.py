afa917a9d0ef73d2fc4d18f3c4c39c0520304460
guwenpeng <wenpeng.gu@huilianyi.com>
2019-09-02 14:25:33 +0800

    优化body参数

--- SendMQ.py
+++ SendMQ.py
@@ -21,7 +21,7 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host=ip_addr, por
 channel = connection.channel()
 # 声明一个队列，生产者和消费者都要声明一个相同的队列，用来防止万一某一方挂了，另一方能正常运行
 channel.queue_declare(queue='XPMS_DEVELOPER_MEMBER',passive=True)
-body = '{"CrsResvNo":"2011018190829150321007006X02SS5O","ResNo":"2011018190829150321007006X02SS5O","MemberId":"031106173","MemberLevel":"I","Date":"2019/8/29 15:03:37","HotelId":"2011018"}'
+body = '{"EventDataString":"@PubId:06b7b492-9a48-4792-97e2-9b4a8a6234c1@{\\"PubId\\":\\"06b7b492-9a48-4792-97e2-9b4a8a6234c1\\",\\"EventCode\\":\\"DEVELOPER_MEMBER\\",\\"Target\\":null,\\"KeyWord1\\":\\"MOCKNSTNQLQG\\",\\"KeyWord2\\":\\"031112339\\",\\"EventTime\\":\\"2019-08-15T16:53:10\\",\\"MessageContent\\":\\"{\\\\\\"CrsResvNo\\\\\\":\\\\\\"\\\\\\",\\\\\\"ResNo\\\\\\":\\\\\\"MOCKNSTNQLQG\\\\\\",\\\\\\"MemberId\\\\\\":\\\\\\"031112339\\\\\\",\\\\\\"MemberLevel\\\\\\":\\\\\\"P\\\\\\",\\\\\\"Date\\\\\\":\\\\\\"2019/8/15 16:53:10\\\\\\",\\\\\\"HotelId\\\\\\":\\\\\\"1434\\\\\\"}\\"}","HittedRuleId":327,"TaskTypeCode":"XPMS_DEVELOPER_MEMBER","QueueServiceHostName":"10.1.249.250","QueueServiceVhostName":"XPMS"}'
 for i in range(1,100):
     # 交换机; 队列名,写明将消息发往哪个队列; 消息内容
     # routing_key在使用匿名交换机的时候才需要指定，表示发送到哪个队列
